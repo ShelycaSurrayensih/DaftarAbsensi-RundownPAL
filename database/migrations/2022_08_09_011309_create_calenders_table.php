e33f304ef97eed16a647b64e1347ec9e3f8dec63
Irsyadha Alfyrdhousi <irsyadhaalfyrdhousi@gmail.com>
2022-08-09 09:19:27 +0700

    Ralat Calender

--- database/migrations/2022_08_09_011309_create_calenders_table.php
+++ database/migrations/2022_08_09_011309_create_calenders_table.php
@@ -13,7 +13,7 @@ class CreateCalendersTable extends Migration
      */
     public function up()
     {
-        Schema::create('calender', function (Blueprint $table) {
+        Schema::create('calenders', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->text('description')->nullable();
