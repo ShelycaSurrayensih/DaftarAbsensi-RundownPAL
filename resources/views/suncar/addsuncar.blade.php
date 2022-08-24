987bbf7054d03d8047ca19aa590a6c85c6693ef3
Irsyadha Alfyrdhousi <irsyadhaalfyrdhousi@gmail.com>
2022-08-24 10:34:00 +0700

    Update Pencarian pada Suncar

--- resources/views/suncar/addsuncar.blade.php
+++ resources/views/suncar/addsuncar.blade.php
@@ -19,18 +19,16 @@
                         </div>
                         <div class="form row">
                             <label for="idRundowns" class="col-sm-12  col-form-label">Acara</label>
-                            <div class="col-sm-12 col-md-10">
-                                {{-- <div class="input-group">
+                                <div class="input-group">
                                     <input id="rundown_namaAcara" type="text" class="form-control" readonly="" required>
                                     <input id="idRundowns" type="hidden" name="idRundowns" value="{{ ('idRundowns') }}" required readonly="">
-                                    <button type="button" class="btn btn-info btn-secondary" data-toggle="modal" data-target="#myModal"><b>Cari Acara </b><span class="fa fa-search"></span></button>
-                                </div> --}}
-                                <select name="idRundowns" id="idRundowns">
+                                    <button type="button" class="btn btn-info btn-secondary" data-bs-toggle="modal" data-bs-target="#myModal"><b>Cari Acara </b><span class="fa fa-search"></span></button>
+                                </div>
+                                {{-- <select name="idRundowns" id="idRundowns">
                                     @foreach ($rundown as $run)
                                     <option value="{{ $run->idRundowns}}">{{ $run->namaAcara}}</option>
                                     @endforeach
-                                </select>
-                            </div>
+                                </select> --}}
                         </div>
                         <div class="col-sm-12">
                             <div class="form-group form-group-default">
@@ -66,16 +64,15 @@
     </div>
 </div>
 </div>
-
 <!--End Modal Add-->
 
-{{-- <!-- Modal Rundowns -->
+<!-- Modal Rundowns -->
 <div class="modal fade bd-example-modal-lg" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" >
     <div class="modal-dialog modal-lg" role="document" >
         <div class="modal-content" style="background: #fff;">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Cari Acara</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <button type="button" class="btn-close" data-bs-dismiss="modal">
                 <span aria-hidden="true">&times;</span>
                 </button>
             </div>
@@ -89,9 +86,10 @@
                             <th>Lokasi</th>
                         </tr>
                     </thead>
+
                     <tbody>
                         @foreach($rundown as $r)
-                        <tr class="pilih_rundown" data-idRundowns="<?php echo $r->idRundowns; ?>" data-rundowns_namaAcara="<?php echo $r->namaAcara; ?>" >
+                        <tr class="pilih_rundown" r-id_Rundowns="<?php echo $r->idRundowns; ?>" r-rundown_namaAcara="<?php echo $r->namaAcara; ?>" r-rundown_lokasi="<?php echo $r->lokasi; ?>" >
                             <td>{{$loop->iteration}}</td>
                             <td>{{$r->kodeRundowns}}</td>
                             <td>{{$r->namaAcara}}</td>
@@ -104,4 +102,4 @@
         </div>
     </div>
 </div>
-<!-- End Modal Rundowns --> --}}
+<!-- End Modal Rundowns -->
