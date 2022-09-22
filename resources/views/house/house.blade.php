13cb32383609bac23baea3d8abc8db983e5603ef
Irsyadha Alfyrdhousi <irsyadhaalfyrdhousi@gmail.com>
2022-09-22 13:30:05 +0700

    Update Style House

--- resources/views/house/house.blade.php
+++ resources/views/house/house.blade.php
@@ -53,8 +53,8 @@
                     <div class="card-body">
                         <div class="media mb-5 align-items-center event-list">
                             <div class="p-3 text-center rounded me-3 date-bx bgl-primary">
-                                <h2 class="mb-0 text-black">3</h2>
-                                <h5 class="mb-1 text-black">Wed</h5>
+                                <i class="fa fa-plus" style="font-size:25px;color:rgb(37, 37, 169)"></i>
+                                {{-- <a class="fa-sharp fa-solid fa-pen"></a> --}}
                             </div>
                             <div class="media-body px-0">
                                 <h6 class="mt-0 mb-3 fs-14"><a class="text-black" href="events.html">Live Concert Choir
@@ -72,27 +72,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="media mb-5 align-items-center event-list">
-                            <div class="p-3 text-center rounded me-3 date-bx bgl-primary">
-                                <h2 class="mb-0 text-black">16</h2>
-                                <h5 class="mb-1 text-black">Tue</h5>
-                            </div>
-                            <div class="media-body px-0">
-                                <h6 class="mt-0 mb-3 fs-14"><a class="text-black" href="events.html">Beautiful Fireworks
-                                        Show In The
-                                        New Year Night</a></h6>
-                                <ul class="fs-14 list-inline mb-2 d-flex justify-content-between">
-                                    <li>Ticket Sold</li>
-                                    <li>431/650</li>
-                                </ul>
-                                <div class="progress mb-0" style="height:4px; width:100%;">
-                                    <div class="progress-bar bg-warning progress-animated"
-                                        style="width:50%; height:8px;" role="progressbar">
-                                        <span class="sr-only">60% Complete</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+
                         <div class="media mb-0 align-items-center event-list">
                             <div class="p-3 text-center rounded me-3 date-bx bgl-success">
                                 <h2 class="mb-0 text-black">28</h2>
@@ -115,6 +95,13 @@
                             </div>
                         </div>
                     </div>
+                    {{-- @foreach ($rundown as $r)
+                    <div class="modal fade" id="detailModalrundown{{ $r->idRundowns }}" tabindex="-1" role="dialog" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+
+
+                    @endforeach --}}
                     <div class="card-footer justify-content-between border-0 d-flex fs-14">
                         <span>5 events more</span>
                         <a href="javascript:void(0);" class="text-primary">View more <i
