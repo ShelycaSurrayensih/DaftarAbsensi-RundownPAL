10f8f4887ceafc90846c87e9b398df6ff11d143a
yocki24 <yockifebrianto@gmail.com>
2022-09-26 14:53:11 +0700

    update qrcode

--- app/Http/Controllers/QrCodeGeneratorController.php
+++ app/Http/Controllers/QrCodeGeneratorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Rundown;
 
 class QrCodeGeneratorController extends Controller
 {
@@ -13,7 +14,8 @@ class QrCodeGeneratorController extends Controller
      */
     public function index()
     {
-        return view('qrCode');
+        $rundown = Rundown::all();
+        return view('qrCode', compact('rundown'));
     }
 
     /**
--- resources/views/qrCode.blade.php
+++ resources/views/qrCode.blade.php
@@ -1,1 +1,5 @@
-{!! QrCode::size(250)->generate('learning-points.in') !!}
+@foreach ($rundown as $r)
+    {{-- {!! QrCode::size(250)->generate(Request::url( route('absensi.absensi', $r->idRundowns))); !!} --}}
+    {!! QrCode::size(250)->generate(url('absensi/'.$r->idRundowns)); !!}
+
+@endforeach
